231bfdc2c5215777538d268f2b588ec321668731
m-vngrdv <isocialnaya@mail.ru>
2020-09-02 08:55:40 +0300

    Часть адаптива для главной страницы

--- markup/components/sidebar/sidebar.js
+++ markup/components/sidebar/sidebar.js
@@ -1,1 +1,17 @@
+let links = document.querySelectorAll('.sidebar__link');
 
+document.addEventListener("click", function(event) {
+    if(event.target.classList.contains('sidebar__link')) {
+        let idPanel = event.target.getAttribute("data-panel"),
+            activeSidebar = document.querySelector('.sidebar__link-active'),
+            activePanel = document.querySelector('.dashboard-panel-active');
+
+        activeSidebar.classList.remove('sidebar__link-active');
+        event.target.classList.add('sidebar__link-active');
+
+
+        activePanel.classList.remove('dashboard-panel-active');
+
+        document.getElementById(idPanel.replace('#', '')).classList.add('dashboard-panel-active');
+    }
+});
